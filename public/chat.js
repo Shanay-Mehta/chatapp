b4a65655d86202272bae0859e650fdd4bcfbfb32
Shanay-Mehta <f20211322@goa.bits-pilani.ac.in>
2022-09-24 13:44:44 +0530

    changed server url for request

--- public/chat.js
+++ public/chat.js
@@ -1,5 +1,5 @@
 // Make connection
-var socket = io.connect("http://localhost:4000");
+var socket = io.connect("wss://shanay-chatapp.herokuapp.com/");
 
 // Query DOM
 var message = document.getElementById("message"),
