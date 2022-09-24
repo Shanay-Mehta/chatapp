4a75293edc0417144f9c6e3f41507828c9d8a81a
Shanay-Mehta <f20211322@goa.bits-pilani.ac.in>
2022-09-24 13:41:06 +0530

    changed port

--- public/chat.js
+++ public/chat.js
@@ -6,12 +6,18 @@ var message = document.getElementById("message"),
   handle = document.getElementById("handle"),
   btn = document.getElementById("send"),
   output = document.getElementById("output");
+chatWindow = document.getElementById("chat-window");
 
 // Emit events
 btn.addEventListener("click", function () {
+  const date = new Date();
+  const hour = date.getHours();
+  const minute = date.getMinutes();
   socket.emit("chat", {
     message: message.value,
     handle: handle.value,
+    hours: hour,
+    minutes: minute,
   });
   message.value = "";
 });
@@ -19,5 +25,14 @@ btn.addEventListener("click", function () {
 // Listen for events
 socket.on("chat", function (data) {
   output.innerHTML +=
-    "<p><strong>" + data.handle + ": </strong>" + data.message + "</p>";
+    "<p><strong>" +
+    data.handle +
+    ": </strong>" +
+    data.message +
+    "</p><p>" +
+    data.hours +
+    ": " +
+    data.minutes +
+    "</p><br>";
+  chatWindow.scrollTop = 16000;
 });
